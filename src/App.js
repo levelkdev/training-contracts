c59e4032918c083dd6fe96995b605c60e81757a0
Chris Whinfrey <cwhinfrey@gmail.com>
2018-04-25 12:14:54 -0400

    get tokens

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 //import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
 //import Class from '../build/contracts/Class.json'
 import TokenMaster from '../build/contracts/TokenMaster.json'
+import StudentToken from '../build/contracts/StudentToken.json'
 import getWeb3 from './utils/getWeb3'
 import contract from 'truffle-contract'
 
@@ -70,36 +71,6 @@ class App extends Component {
       })
     })
 
-    /*
-     * SMART CONTRACT EXAMPLE
-     *
-     * Normally these functions would be called in the context of a
-     * state management library, but for convenience I've placed them here.
-     */
-
-/*    const contract = require('truffle-contract')
-    const simpleStorage = contract(SimpleStorageContract)
-    simpleStorage.setProvider(this.state.web3.currentProvider)
-
-    // Declaring this for later so we can chain functions on SimpleStorage.
-    var simpleStorageInstance
-
-    // Get accounts.
-    this.state.web3.eth.getAccounts((error, accounts) => {
-      simpleStorage.deployed().then((instance) => {
-        simpleStorageInstance = instance
-
-        // Stores a given value, 5 by default.
-        return simpleStorageInstance.set(5, {from: accounts[0]})
-      }).then((result) => {
-        // Get the value from the contract to prove it worked.
-        return simpleStorageInstance.get.call(accounts[0])
-      }).then((result) => {
-        // Update state with the result.
-        return this.setState({ storageValue: result.c[0] })
-      })
-    })*/
-
   }
 
   taTips () {
@@ -127,14 +98,19 @@ class App extends Component {
         tokenMasterInstance = instance
         return tokenMasterInstance.getTokenList()
       }).then((listings) => {
-        return Promise.all(listings.map(entry => this.getEntry(tokenMasterInstance, entry)))
-      }).then((listings) => {
         console.log("set state " + listings)
+        return Promise.all(listings.map(tokenAddress => this.getStudentToken(tokenAddress)))
+      }).then((listings) => {
+        this.state.tokenList.forEach(token => this.logToken(token))
         //this.setState({listings: listings})
       }).catch((error) => {
         console.log(error)
       })
     })
+  }
+  
+  logToken(token) {
+    console.log("name: " + token.name + "symbol: " + token.symbol + "address: " + token.address)
   }
 
   getTokenMaster() {
@@ -143,15 +119,19 @@ class App extends Component {
     return tokenMaster.deployed()
   }
 
-  getEntry(tokenMasterInstance, entry) {
-    return tokenMasterInstance.getTokenList().then((listing) => {
-      console.log(listing)
-      this.setState({listings: listing})
-      return {
-        tokenList: entry,
-        name: window.web3.toAscii(entry),
-        address: listing
-      }
+  getStudentToken(address) {
+    const studentTokenContract = contract(StudentToken)
+    studentTokenContract.setProvider(this.state.web3.currentProvider)
+    var studentToken = studentTokenContract.at(address)
+    var newToken = {address: address}
+    var tokenList = this.state.tokenList
+    return studentToken.name().then((name) => {
+      newToken.name = name
+      return studentToken.symbol()
+    }).then((symbol) => {
+      newToken.symbol = symbol
+      tokenList.push(newToken)
+      this.setState({tokenList: tokenList})
     })
   }
 
